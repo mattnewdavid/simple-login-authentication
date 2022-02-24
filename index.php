6e112ef8c5a82530ff33e65b3f17f93cb9093a9e
Matthew David <matthew44olawoyin@gmail.com>
2022-02-24 14:47:07 +0100

    Fonts and other changes

--- index.php
+++ index.php
@@ -3,21 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Expert</title>
+    <title>Login Authentication</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
     
         <form method="post" action="signup.php">
+            <h2>Sign up</h2>
             <input name="name" type="text" placeholder="Name"><br>
             <input name="email" type="text" placeholder="Email"><br>
             <input name="password" type="password" placeholder="Password"><br>
             <input name="passwordcheck" type="password" placeholder="Confirm Password"><br>
             <input class="submit" name="submit" type="submit" value="Sign-up"><br>
         </form>
-        
+
         <form method="post" action="login.php">
+            <h2>Log in</h2>
             <input name="name" type="text" placeholder="Name"><br>
             <input name="email" type="text" placeholder="Email"><br>
             <input name="password" type="password" placeholder="Password"><br>
